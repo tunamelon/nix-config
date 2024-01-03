1cfca9af229d92c74a9e9dedf72ab8e1d88fc960
Tuna <tuna@sodamelon.com>
2024-01-03 10:03:03 +0000

    Syntax fix aliases pi

--- modules/tunapi-0/aliases.nix
+++ modules/tunapi-0/aliases.nix
@@ -3,8 +3,8 @@
   nix-switch = "home-manager switch --flake github:tunamelon/nix-config#tunapi-0 && exec zsh";
 
   # Ripgrep alias
-#  alias search=rg -p --glob '!node_modules/*'  $@
+#  alias search = "rg -p --glob '!node_modules/*'  $@";
 
   # Always color ls and group directories
-  alias ls='ls --color=auto'
+  alias ls = "ls --color=auto";
 }

8a5fe901feca3dcef6c68d95db4d81bd3ab76c6b
Tuna <tuna@sodamelon.com>
2024-01-04 13:22:35 +0000

    New pull build process

--- modules/darwin/aliases.nix
+++ modules/darwin/aliases.nix
@@ -1,5 +1,6 @@
 { config, pkgs, lib, ... }:
 {
-  nix-switch = "nix run github:tunamelon/nix-config#build-switch && exec zsh";
+  #nix-switch = "nix run github:tunamelon/nix-config#build-switch && exec zsh";
+  nix-switch = "cd /tmp && git init git@github.com:tunamelon/nix-config.git && cd nix-config && .#build-switch && exec zsh && cd ~/ && rm -rf /tmp/nix-config";
   test = "echo my bb ty!";
 }

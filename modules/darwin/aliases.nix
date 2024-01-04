c8c5aa1a7a995535079004a5b594ffe36b5bcd72
Tuna <tuna@sodamelon.com>
2024-01-04 14:43:51 +0000

    Typo

--- modules/darwin/aliases.nix
+++ modules/darwin/aliases.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 {
   #nix-switch = "nix run github:tunamelon/nix-config#build-switch && exec zsh";
-  nix-switch="pushd . > /dev/null && echo 'Cloning git repo...' && cd /tmp/ && git clone git@github.com:tunamelon/nix-config.git && cd nix-config && nix run .#build-switch; popd > /dev/null; rm -rf /tmp/nix-config; exec zsh"
+  nix-switch="pushd . > /dev/null && echo 'Cloning git repo...' && cd /tmp/ && git clone git@github.com:tunamelon/nix-config.git && cd nix-config && nix run .#build-switch; popd > /dev/null; rm -rf /tmp/nix-config; exec zsh";
   test = "echo my bb ty!";
 }

4af429c026645b6869da3ba08fe376b57856d44a
Tuna <tuna@sodamelon.com>
2024-01-10 09:07:44 +0000

    Ammend hosts pi4i pi5

--- hosts/tunapi-4i/default.nix
+++ hosts/tunapi-4i/default.nix
@@ -3,11 +3,11 @@ let user = "tuna";
     keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk8iAnIaa1deoc7jw8YACPNVka1ZFJxhnU4G74TmS+p" ]; in
 {
   imports = [
-#    ../../modules/tunapi-0/secrets.nix
-#    ../../modules/tunapi-0/disk-config.nix
+#    ../../modules/tunapi-4i/secrets.nix
+#    ../../modules/tunapi-4i/disk-config.nix
 #    ../../modules/shared
 #    ../../modules/shared/cachix
-     ../../modules/tunapi-0/home-manager.nix
+     ../../modules/tunapi-4i/home-manager.nix
 #    agenix.nixosModules.default
     
   ];
--- hosts/tunapi-5/default.nix
+++ hosts/tunapi-5/default.nix
@@ -3,11 +3,11 @@ let user = "tuna";
     keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk8iAnIaa1deoc7jw8YACPNVka1ZFJxhnU4G74TmS+p" ]; in
 {
   imports = [
-#    ../../modules/tunapi-0/secrets.nix
-#    ../../modules/tunapi-0/disk-config.nix
+#    ../../modules/tunapi-5/secrets.nix
+#    ../../modules/tunapi-5/disk-config.nix
 #    ../../modules/shared
 #    ../../modules/shared/cachix
-     ../../modules/tunapi-0/home-manager.nix
+     ../../modules/tunapi-5/home-manager.nix
 #    agenix.nixosModules.default
     
   ];
